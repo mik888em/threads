efb2adcc1e13af987ffa082e00ddc4b31b6f0c65
Mikhail Eliseev <67550751+mik888em@users.noreply.github.com>
2025-09-26 09:24:14 +0300

    fix(aggregation): синхронизировать счётчики с инсайтами

--- src/threads_metrics/aggregation.py
+++ src/threads_metrics/aggregation.py
@@ -18,19 +18,43 @@ def aggregate_posts(
         post_id = str(raw_post_id)
         insight = insights.get(post_id, {})
         has_insight = post_id in insights
+
+        post_like_count = post.get("like_count")
+        if post_like_count is None:
+            post_like_count = 0
+        post_reply_count = post.get("reply_count")
+        if post_reply_count is None:
+            post_reply_count = 0
+        post_repost_count = post.get("repost_count")
+        if post_repost_count is None:
+            post_repost_count = 0
+
+        like_value = (
+            insight.get("likes", post_like_count) if has_insight else post_like_count
+        )
+        reply_value = (
+            insight.get("replies", post_reply_count)
+            if has_insight
+            else post_reply_count
+        )
+        repost_value = (
+            insight.get("reposts", post_repost_count)
+            if has_insight
+            else post_repost_count
+        )
         aggregated.append(
             {
                 "account_name": post.get("account_name"),
                 "post_id": post_id,
                 "permalink": post.get("permalink"),
                 "text": post.get("text"),
-                "like_count": post.get("like_count", 0),
-                "repost_count": post.get("repost_count", 0),
-                "reply_count": post.get("reply_count", 0),
+                "like_count": like_value,
+                "repost_count": repost_value,
+                "reply_count": reply_value,
                 "views": insight.get("views") if has_insight else None,
-                "likes": insight.get("likes", post.get("like_count", 0)),
-                "replies": insight.get("replies", post.get("reply_count", 0)),
-                "reposts": insight.get("reposts", post.get("repost_count", 0)),
+                "likes": like_value,
+                "replies": reply_value,
+                "reposts": repost_value,
                 "quotes": insight.get("quotes") if has_insight else None,
                 "shares": insight.get("shares") if has_insight else None,
             }
